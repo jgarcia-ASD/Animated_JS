f6e670aef7ea28f76931f463bcccce0cb4736ac4
jgarcia-ASD <jgarcia@grupoasd.com>
2023-04-19 16:13:42 -0500

    Despues der ver el segundo video de la unidad 3

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -4,6 +4,11 @@ const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
+//hacemos una funcion para convertir los radianes a grados y facilitarnos la codificacion
+const agrados = (grados) => {
+  return grados / 180 * Math.PI;
+}
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -11,21 +16,34 @@ const sketch = () => {
 
     context.fillStyle = 'black';
 
-    const x = width * 0.5;
-    const y = height * 0.5;
-    const w = width * 0.3;
-    const h = height * 0.3;
+    const cx = width * 0.5;
+    const cy = height * 0.5;
+    const w = width * 0.01;
+    const h = height * 0.1;
+    let x, y;
 
-    //guardamos como tenemos ubicado nuestra ojo por que las transformaciones sin acomulativas
-    context.save();
-    context.translate(x, y);
-    context.rotate(0.3);
+    const num = 12;
+    const radio = width * 0.3;
 
-    context.beginPath();
-    context.rect(-w * 0.5, -h * 0.5, w, h);
-    context.fill();
-    //asi evitamos que se nos acumulen las tranformaciones
-    context.restore();
+    for (let i = 0; i < num; i++) {
+      const slice = agrados(360 / num);
+      const angulo = slice * i;
+
+      x = cx + radio * Math.sin(angulo);
+      y = cy + radio * Math.cos(angulo);
+
+      //guardamos como tenemos ubicado nuestra ojo por que las transformaciones sin acomulativas
+      context.save();
+      context.translate(x, y);
+      context.rotate(-angulo);
+
+      context.beginPath();
+      context.rect(-w * 0.5, -h * 0.5, w, h);
+      context.fill();
+      //asi evitamos que se nos acumulen las tranformaciones
+      context.restore();
+      
+    }
   };
 };
 
