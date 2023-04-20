994b3f434feab34456881b31c2d6a95403f12aa7
jgarcia-ASD <jgarcia@grupoasd.com>
2023-04-20 09:03:21 -0500

    Despues de ver el segundo video de la unidad 4

--- sketches/sketch-03.js
+++ sketches/sketch-03.js
@@ -3,10 +3,18 @@ const random = require('canvas-sketch-util/random');
 
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true
 };
 
-const sketch = ({ context, width, height }) => {
+//si no tenemos acceso a canvas-sketch
+const animate = () => {
+  console.log('domestika')
+  requestAnimationFrame(animate);
+}
+//y ahora la llamamos
+//animate ()
+const sketch = ({ width, height }) => {
   const agentes = [];
 
   for (let i = 0; i < 40; i++) {
@@ -21,14 +29,16 @@ const sketch = ({ context, width, height }) => {
     context.fillRect(0, 0, width, height);
 
     agentes.forEach(Angente => {
+      Angente.update();
       Angente.draw(context);
+      Angente.limites(width, height);
     });
   };
 };
 
 canvasSketch(sketch, settings);
 
-class Point {
+class Vector {
   constructor(x, y){
     this.x = x;
     this.y = y;
@@ -37,15 +47,32 @@ class Point {
 
 class Angente {
   constructor(x, y){
-    this.pos = new Point(x,y);
-    this.radio = 10;
+    this.pos = new Vector(x,y);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+    this.radio = random.range(4, 12);
   }
 
+
+  limites(width, height){
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1;
+    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
+  }
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
   draw(context) {
-    context.fillStyle = 'black';
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+
+    context.lineWidth = 4;
+
     context.beginPath();
-      context.arc(this.pos.x, this.pos.y, this.radio, 0, Math.PI * 2);
-      context.fill();
+    context.arc(0, 0, this.radio, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+
+    context.restore();
   }
 }
 
